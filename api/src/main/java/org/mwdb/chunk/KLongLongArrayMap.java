ed7b84a167d92af9813e27e7657e9cd1fdf3f4fb
Francois Fouquet <fouquet.f@gmail.com>
2016-03-09 16:51:35 +0100

    continue to push on documentation

--- api/src/main/java/org/mwdb/chunk/KLongLongArrayMap.java
+++ api/src/main/java/org/mwdb/chunk/KLongLongArrayMap.java
@@ -2,16 +2,30 @@ package org.mwdb.chunk;
 
 public interface KLongLongArrayMap {
 
+    /**
+     * Get all associated values to the current param key
+     *
+     * @param key key to be retrieve
+     * @return array of associated values[]
+     */
     long[] get(long key);
 
     /**
      * Add the tuple key/value to the map.
+     * This map allows keys conflicts.
+     * In other words, one key can be mapped to various values.
      *
      * @param key   to insert key
      * @param value to insert value
      */
     void put(long key, long value);
 
+    /**
+     * Remove the current K/V tuple from the map
+     *
+     * @param key   to delete key
+     * @param value to delete value
+     */
     void remove(long key, long value);
 
     /**
