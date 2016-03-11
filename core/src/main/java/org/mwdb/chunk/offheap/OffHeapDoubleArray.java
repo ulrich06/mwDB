ed3d78c3235c8857fa69b9cc2098f135f0c578b9
Thomas Hartmann <thomi.hartmann@gmail.com>
2016-03-11 11:04:07 +0100

    -> Refactoring of OffHeap version

--- core/src/main/java/org/mwdb/chunk/offheap/OffHeapDoubleArray.java
+++ core/src/main/java/org/mwdb/chunk/offheap/OffHeapDoubleArray.java
@@ -6,7 +6,7 @@ import org.mwdb.utility.Unsafe;
 /**
  * @ignore ts
  */
-public class OffHeapLongArray {
+public class OffHeapDoubleArray {
 
     private static final sun.misc.Unsafe unsafe = Unsafe.getUnsafe();
 
@@ -25,19 +25,19 @@ public class OffHeapLongArray {
         //reset the segment with -1
         unsafe.setMemory(newBiggerMemorySegment, nextCapacity * 8, (byte) Constants.OFFHEAP_NULL_PTR);
         //copy previous memory segment content
-        unsafe.copyMemory(addr,newBiggerMemorySegment, previousCapacity * 8);
+        unsafe.copyMemory(addr, newBiggerMemorySegment, previousCapacity * 8);
         //free the previous
         unsafe.freeMemory(addr);
         //return the newly created segment
         return newBiggerMemorySegment;
     }
 
-    public static void set(final long addr, final long index, final long valueToInsert) {
-        unsafe.putLong(addr + index * 8, valueToInsert);
+    public static void set(final long addr, final long index, final double valueToInsert) {
+        unsafe.putDouble(addr + index * 8, valueToInsert);
     }
 
-    public static long get(final long addr, final long index) {
-        return unsafe.getLong(addr + index * 8);
+    public static double get(final long addr, final long index) {
+        return unsafe.getDouble(addr + index * 8);
     }
 
     public static void free(final long addr) {
