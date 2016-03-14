51d484e869763c3beaaa1ba3ce1998087fcd28f5
Thomas Hartmann <thomi.hartmann@gmail.com>
2016-03-14 18:52:03 +0100

    * tiny test

--- core/src/test/java/chunk/FixedStackTest.java
+++ core/src/test/java/chunk/FixedStackTest.java
@@ -1,7 +1,34 @@
 package chunk;
 
-/**
- * Created by thomas on 11/03/16.
- */
+import org.junit.Assert;
+import org.junit.Test;
+import org.mwdb.Constants;
+import org.mwdb.chunk.KStack;
+import org.mwdb.chunk.heap.FixedStack;
+import org.mwdb.chunk.offheap.OffHeapFixedStack;
+
 public class FixedStackTest {
+    private static final int CAPACITY = 15;
+
+
+    @Test
+    public void heapFixedStackTest() {
+        test(new FixedStack(CAPACITY));
+    }
+
+    @Test
+    public void offHeapFixedStackTest() {
+        test(new OffHeapFixedStack(CAPACITY, Constants.OFFHEAP_NULL_PTR));
+    }
+
+    public void test(KStack stack) {
+        for (int i = 0; i < 3; i++) {
+            Assert.assertTrue(stack.dequeueTail() == CAPACITY - (i + 1));
+        }
+
+        System.out.println((stack.dequeue(12)));
+        System.out.println((stack.enqueue(14)));
+        System.out.println((stack));
+
+    }
 }
