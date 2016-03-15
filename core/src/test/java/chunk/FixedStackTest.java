902029491b9c44361150729a3698f3ab0fef7cef
Thomas Hartmann <thomi.hartmann@gmail.com>
2016-03-15 11:26:02 +0100

    * tests + fixes for stack

--- core/src/test/java/chunk/FixedStackTest.java
+++ core/src/test/java/chunk/FixedStackTest.java
@@ -22,13 +22,33 @@ public class FixedStackTest {
     }
 
     public void test(KStack stack) {
-        for (int i = 0; i < 3; i++) {
+        // stack is initially full, dequeue until empty
+        for (int i = 0; i < CAPACITY; i++) {
             Assert.assertTrue(stack.dequeueTail() == CAPACITY - (i + 1));
         }
-
-        System.out.println((stack.dequeue(12)));
-        System.out.println((stack.enqueue(14)));
-        System.out.println((stack));
+        // insert some values again
+        Assert.assertTrue(stack.enqueue(0));
+        Assert.assertTrue(stack.enqueue(1));
+        Assert.assertTrue(stack.enqueue(2));
+        Assert.assertTrue(stack.enqueue(3));
+        Assert.assertTrue(stack.enqueue(4));
+        // dequeue tail
+        Assert.assertTrue(stack.dequeueTail() == 0);
+        // enqueue
+        Assert.assertTrue(stack.enqueue(5));
+        // dequeue index
+        Assert.assertTrue(stack.dequeue(2));
+        // dequeue tail
+        Assert.assertTrue(stack.dequeueTail() == 1);
+        Assert.assertTrue(stack.dequeueTail() == 3);
+        // dequeue invalid index
+        Assert.assertFalse(stack.dequeue(2));
+        Assert.assertFalse(stack.dequeue(1));
+        Assert.assertFalse(stack.dequeue(0));
+        // dequeue valid index
+        Assert.assertTrue(stack.dequeue(4));
+        // dequeue tail
+        Assert.assertTrue(stack.dequeueTail() == 5);
 
     }
 }
