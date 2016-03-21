2fae6e5c0b14296ede762d0b34d522a7dd71a1ab
Francois Fouquet <fouquet.f@gmail.com>
2016-03-21 11:40:21 +0100

    add the pom

--- plugins/ml/src/test/java/ml/MeanTest.java
+++ plugins/ml/src/test/java/ml/MeanTest.java
@@ -5,7 +5,7 @@ import org.mwdb.KGraph;
 public class MeanTest {
 
     public void test() {
-        //KGraph graph = GraphBuilder.builder().withScheduler(new NoopScheduler()).buildGraph();
+        KGraph graph = GraphBuilder.builder().withScheduler(new NoopScheduler()).buildGraph();
     }
 
 }
