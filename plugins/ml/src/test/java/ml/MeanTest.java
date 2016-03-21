3bf323fe0f925e559b40a3936508fec1f4c1341d
Francois Fouquet <fouquet.f@gmail.com>
2016-03-21 11:39:01 +0100

    continue the mean inferred node

---
+++ plugins/ml/src/test/java/ml/MeanTest.java
@@ -0,0 +1,11 @@
+package ml;
+
+import org.mwdb.KGraph;
+
+public class MeanTest {
+
+    public void test() {
+        //KGraph graph = GraphBuilder.builder().withScheduler(new NoopScheduler()).buildGraph();
+    }
+
+}
