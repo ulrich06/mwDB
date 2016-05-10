b959cda01cede265f11ebf7acbd471be1df97202
Assaad Moawad <assaad.moawad@uni.lu>
2016-05-10 13:17:24 +0200

    test reccursion

---
+++ plugins/ml/src/test/java/ml/TestTMP.java
@@ -0,0 +1,126 @@
+package ml;
+
+import org.mwg.Callback;
+import org.mwg.Graph;
+import org.mwg.GraphBuilder;
+import org.mwg.Node;
+import org.mwg.task.*;
+import org.mwg.core.NoopScheduler;
+
+import java.util.Arrays;
+
+/**
+ * Created by assaad on 10/05/16.
+ */
+public class TestTMP {
+
+
+    public static void main(String[] args) {
+        Graph graph = GraphBuilder.builder().withScheduler(new NoopScheduler()).build();
+
+        graph.connect(new Callback<Boolean>() {
+            @Override
+            public void on(Boolean result) {
+                Node root = graph.newNode(0,0);
+                root.set("name","root");
+                root.set("enter",true);
+
+                final long id = root.id();
+
+                Node n1 = graph.newNode(0,0);
+                n1.set("name","n1");
+                n1.set("enter",true);
+
+                Node n2 = graph.newNode(0,0);
+                n2.set("name","n2");
+                n2.set("enter",false);
+
+
+                Node n3 = graph.newNode(0,0);
+                n3.set("name","n3");
+                n3.set("enter",false);
+
+
+                root.add("fils",n1);
+                root.add("fils",n2);
+                root.add("fils",n3);
+
+                Node n4 = graph.newNode(0,0);
+                n4.set("name","n4");
+                n4.set("enter",true);
+
+
+                n1.add("fils",n4);
+
+                Task creationTask = graph.newTask().then(new TaskAction() {
+                    @Override
+                    public void eval(TaskContext context) {
+                        Node node = (Node) context.getVariable("starterNode");
+                        System.out.println("Creation: " + node);
+                    }
+                });
+
+
+                final int[] recursionNb = new int[]{0};
+                Task traverse = graph.newTask();
+                traverse.then(new TaskAction() {
+                    @Override
+                    public void eval(TaskContext context) {
+                        recursionNb[0]++;
+                    }
+                }).fromVar("starterNode").traverse("fils").select(new TaskFunctionSelect() {
+                    @Override
+                    public boolean select(Node node) {
+                        return (boolean) node.get("enter");
+                    }
+                }).asVar("childNode")
+                 .ifThen(new TaskFunctionConditional() {
+                    @Override
+                    public boolean eval(TaskContext context) {
+                        Node[] result = (Node[]) context.getVariable("childNode");
+
+                        if(result.length > 0) {
+                            context.setVariable("starterNode",result[0]);
+                            Node starter = (Node) context.getVariable("starterNode");
+                            System.out.println(recursionNb[0] + " 1er ifThen " + starter + " -> " + Arrays.toString(result));
+
+                        }
+                        return result.length > 0;
+                    }
+                },traverse);
+                /*.ifThen(new TaskFunctionConditional() {
+                    @Override
+                    public boolean eval(TaskContext context) {
+
+
+                        Node[] result = (Node[]) context.getVariable("childNode");
+
+                        if(result.length == 0) {
+                            Node starter = (Node) context.getVariable("starterNode");
+                            System.out.println(recursionNb[0] + " 2nd ifThen " + starter);
+                        }
+                        return result.length == 0;
+                    }
+                },creationTask);*/
+
+                Task mainTask = graph.newTask().from(root).asVar("starterNode").wait(traverse).then(new TaskAction() {
+                    @Override
+                    public void eval(TaskContext context) {
+                        //call callbask
+                    }
+                }).wait(creationTask);
+
+                mainTask.execute();
+
+
+
+
+
+
+
+
+
+            }
+        });
+    }
+}
