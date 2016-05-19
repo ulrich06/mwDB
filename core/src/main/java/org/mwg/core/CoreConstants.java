7f2b42ca7c33e81f00c0993c64d3ed199bb91c0d
Grégory Nain <gregory.nain@gmail.com>
2016-05-19 14:18:16 +0200

    Js Fix

--- core/src/main/java/org/mwg/core/CoreConstants.java
+++ core/src/main/java/org/mwg/core/CoreConstants.java
@@ -42,9 +42,8 @@ public class CoreConstants extends org.mwg.Constants {
     /**
      * ChunkFlags
      */
-    public static final short DIRTY_BIT_INDEX = 0;
-
-    public static final int DIRTY_BIT = 1 << DIRTY_BIT_INDEX;
+    //public static final short DIRTY_BIT_INDEX = 0;
+    public static final int DIRTY_BIT = 0x01;
 
     //public static final short REMOVED_BIT_INDEX = 1;
     //public static final int REMOVED_BIT = 1 << REMOVED_BIT_INDEX;
