4fdf9c93e1a8f2d38335a9b2aa0469187b509c8b
Assaad Moawad <assaad.moawad@uni.lu>
2016-06-03 12:01:55 +0200

    update

--- plugins/ml2/src/test/java/org/mwg/ml/common/SVDDecomposeTest.java
+++ plugins/ml2/src/test/java/org/mwg/ml/common/SVDDecomposeTest.java
@@ -1,7 +1,51 @@
 package org.mwg.ml.common;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mwg.ml.common.matrix.Matrix;
+import org.mwg.ml.common.matrix.MatrixEngine;
+import org.mwg.ml.common.matrix.SVDDecompose;
+import org.mwg.ml.common.matrix.blassolver.BlasMatrixEngine;
+import org.mwg.ml.common.matrix.blassolver.SVD;
+
 /**
  * Created by assaad on 03/06/16.
  */
 public class SVDDecomposeTest {
+    @Test
+    public void decompose(){
+        int m = 20;
+        int n = 30;
+        double eps = 1e-7;
+
+        Matrix matA = Matrix.random(m, n, 0, 100);
+
+        // double[] xx = {1,0,0,0,   0,0,0,2,   0,3,0,0,    0,0,0,0   ,2,0,0,0};
+        // KMatrix matA = new Matrix(xx,m,n,KMatrixType.COLUMN_BASED);
+
+
+        MatrixEngine temp= new BlasMatrixEngine();
+
+
+        long timestart, timeend;
+
+        timestart = System.currentTimeMillis();
+        SVDDecompose svd=temp.decomposeSVD(matA,false);
+        timeend = System.currentTimeMillis();
+        //System.out.println(" SVD Factorizarion " + ((double) (timeend - timestart)) / 1000 + " s");
+
+
+        Matrix U = svd.getU();
+        Matrix S = svd.getSMatrix();
+        Matrix Vt = svd.getVt();
+
+        Matrix res = Matrix.multiply(U, S);
+        res = Matrix.multiply(res, Vt);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                Assert.assertTrue(Math.abs(res.get(i, j) - matA.get(i, j)) < eps);
+            }
+        }
+    }
 }
