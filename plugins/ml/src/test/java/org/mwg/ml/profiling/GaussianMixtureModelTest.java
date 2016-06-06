a22c6216ddfb2492ae8cf347966de79d2ee9bf53
Assaad Moawad <assaad.moawad@uni.lu>
2016-06-06 17:17:40 +0200

    update gmm test

--- plugins/ml/src/test/java/org/mwg/ml/profiling/GaussianMixtureModelTest.java
+++ plugins/ml/src/test/java/org/mwg/ml/profiling/GaussianMixtureModelTest.java
@@ -1,9 +1,11 @@
 package org.mwg.ml.profiling;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.mwg.Callback;
 import org.mwg.Graph;
 import org.mwg.GraphBuilder;
+import org.mwg.Type;
 import org.mwg.ml.algorithm.profiling.GaussianGmmNode;
 import org.mwg.core.scheduler.NoopScheduler;
 
@@ -23,21 +25,40 @@ public class GaussianMixtureModelTest {
                 Random rand = new Random();
 
                 GaussianGmmNode node1 = (GaussianGmmNode) graph.newTypedNode(0, 0, "GaussianGmm");
-                node1.set(GaussianGmmNode.LEVEL_KEY,1);
-                node1.set(GaussianGmmNode.WIDTH_KEY,100);
 
-                for (int i = 0; i < 220; i++) {
+                node1.setProperty(GaussianGmmNode.LEVEL_KEY, Type.INT,1);
+                node1.setProperty(GaussianGmmNode.WIDTH_KEY, Type.INT,100);
+
+                double[] sum=new double[3];
+                int total=220;
+
+                for (int i = 0; i < total; i++) {
                     data[0] = 8 + rand.nextDouble() * 4; //avg =10, [8,12]
                     data[1] = 90 + rand.nextDouble() * 20; //avg=100 [90,110]
                     data[2] = -60 + rand.nextDouble() * 20; //avg=-50 [-60,-40]
-                    node1.setTrainingVector(data);
+                    //node1.setTrainingVector(data);
 
-                    node1.learn(new Callback<Boolean>() {
+                    node1.learnVector(data,new Callback<Boolean>() {
                         @Override
                         public void on(Boolean result) {
 
                         }
                     });
+
+                    sum[0]+=data[0];
+                    sum[1]+=data[1];
+                    sum[2]+=data[2];
+                }
+
+                sum[0]= sum[0]/total;
+                sum[1]= sum[1]/total;
+                sum[2]= sum[2]/total;
+
+                double eps=1e-7;
+
+                double[] res= node1.getAvg();
+                for(int i=0;i<3;i++){
+                    Assert.assertTrue(Math.abs(res[i]-sum[i])<eps);
                 }
             }
         });
