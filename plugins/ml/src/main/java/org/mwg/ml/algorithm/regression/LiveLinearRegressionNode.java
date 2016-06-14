cc75ec411e6ad7f5c5b3431c0770640521c65374
Assaad MOAWAD <assaad.moawad@uni.lu>
2016-06-14 19:27:57 +0200

    update ML, still TS not working

--- plugins/ml/src/main/java/org/mwg/ml/algorithm/regression/LiveLinearRegressionNode.java
+++ plugins/ml/src/main/java/org/mwg/ml/algorithm/regression/LiveLinearRegressionNode.java
@@ -9,16 +9,18 @@ import org.mwg.ml.RegressionNode;
 import org.mwg.plugin.NodeFactory;
 import org.mwg.plugin.NodeState;
 
+import java.util.Random;
+
 /**
  * Created by assaad on 14/06/16.
  */
 public class LiveLinearRegressionNode extends AbstractMLNode implements RegressionNode {
 
     public static final String ALPHA_KEY = "ALPHA"; //learning rate
-    public static final double ALPHA_DEF = 0.01;
+    public static final double ALPHA_DEF = 0.0001;
 
     public static final String LAMBDA_KEY = "LAMBDA"; //regularization rate
-    public static final double LAMBDA_DEF = 0.001;
+    public static final double LAMBDA_DEF = 0.00001;
 
     public static final String ITERATION_KEY = "ITERATION"; //Number of iterations on each values
     public static final int ITERATION_DEF = 5;
@@ -27,9 +29,11 @@ public class LiveLinearRegressionNode extends AbstractMLNode implements Regressi
     public static final double THRESHOLD_DEF = 0.01;
 
 
-    private static final String INTERNAL_TOTAL_KEY = "TOTAL_KEY";
-    public static final String WEIGHT_KEY = "_weight";
-    private static final String INTERNAL_WEIGHT_BACKUP_KEY = "_weightbckup";
+    public static final String LAST_ERR_KEY = "_ERR";
+    public static final String WEIGHT_KEY = "_WEIGHT";
+
+    private static final String INTERNAL_TOTAL_KEY = "_TOTAL_KEY";
+    private static final String INTERNAL_WEIGHT_BACKUP_KEY = "_WEIGHTBACKUP";
 
     private static final String MISMATCH_MSG = "Different Imput lengths are not supported";
 
@@ -64,7 +68,6 @@ public class LiveLinearRegressionNode extends AbstractMLNode implements Regressi
                 internalLearn(input, output, callback);
             }
         });
-
     }
 
     public void internalLearn(double[] input, double output, Callback<Boolean> callback) {
@@ -74,26 +77,35 @@ public class LiveLinearRegressionNode extends AbstractMLNode implements Regressi
         double lambda = state.getFromKeyWithDefault(LAMBDA_KEY, LAMBDA_DEF);
         double[] weights = (double[]) state.getFromKey(WEIGHT_KEY);
 
+
+
         if (weights == null) {
             weights = new double[input.length + 1];
+            Random random=new Random();
+            for(int i=0;i<weights.length;i++){
+                weights[i]=random.nextDouble()*0.001;
+            }
         }
 
-        if (input == null) {
-            throw new RuntimeException("Input can't be null");
+        //ToDo test currentErr and update alpha automatically
+        double prevErr = state.getFromKeyWithDefault(LAST_ERR_KEY,0.0);
+        double currErr = calculate(weights, input) - output;
+        if(currErr>prevErr){
         }
 
-        if (weights.length != (input.length + 1)) {
+        state.setFromKey(LAST_ERR_KEY,Type.DOUBLE,currErr);
+
+        if (input == null || weights.length != (input.length + 1)) {
             throw new RuntimeException(MISMATCH_MSG);
         }
         int featuresize = input.length;
 
         for (int j = 0; j < iterations; j++) {
-            double h = calculate(weights, input);
-            double err = alpha * (h - output);
+            double h = calculate(weights, input) - output;
             for (int i = 0; i < featuresize; i++) {
-                weights[i] = weights[i] - alpha * ((h - output) * input[i] - lambda * weights[i]);
+                weights[i] = weights[i] - alpha * (h * input[i] + lambda * weights[i]);
             }
-            weights[featuresize] = weights[featuresize] - err;
+            weights[featuresize] = weights[featuresize] - alpha * h;
         }
 
         double[] bckupWeight = (double[]) state.getFromKey(INTERNAL_WEIGHT_BACKUP_KEY);
@@ -107,8 +119,10 @@ public class LiveLinearRegressionNode extends AbstractMLNode implements Regressi
                 diff = Math.max(diff, Math.abs(weights[i] - bckupWeight[i]));
             }
             double deviation = state.getFromKeyWithDefault(THRESHOLD_KEY, THRESHOLD_DEF);
-            if (diff > deviation) {
-                state = newState(time());
+
+            if (diff>deviation) {
+                state = phasedState();
+                //ToDo test weight here and play with alpha
                 state.setFromKey(WEIGHT_KEY, Type.DOUBLE_ARRAY, weights);
                 state.setFromKey(INTERNAL_WEIGHT_BACKUP_KEY, Type.DOUBLE_ARRAY, weights);
                 state.setFromKey(INTERNAL_TOTAL_KEY, Type.INT, 1);
--- plugins/ml/src/test/java/org/mwg/ml/regression/LiveLinearRegressionTest.java
+++ plugins/ml/src/test/java/org/mwg/ml/regression/LiveLinearRegressionTest.java
@@ -27,19 +27,22 @@ public class LiveLinearRegressionTest {
                 RegressionNode learningNode = (RegressionNode) graph.newTypedNode(0, 0, LiveLinearRegressionNode.NAME);
                 learningNode.add("sensor", MultiSensor);
                 learningNode.set("from", "sensor.temperature; sensor.humidity; sensor.power");
-                learningNode.set(LiveLinearRegressionNode.ALPHA_KEY, 0.01);
+                learningNode.set(LiveLinearRegressionNode.ALPHA_KEY, 0.00001);
+                learningNode.set(LiveLinearRegressionNode.LAMBDA_KEY, 0.00);
+                learningNode.set(LiveLinearRegressionNode.ITERATION_KEY, 10);
                 MultiSensor.add("regression", learningNode);
 
                 final Random random = new Random();
-                final double coef[] = {2, -3, 0.001, -3};
+                final double coef[] = {2, -3, -2, 5};
+                int size = 1000;
 
-                for (int i = 0; i < 500; i++) {
+                for (int i = 0; i < size; i++) {
                     MultiSensor.jump(i + 1, new Callback<Node>() {
                         @Override
                         public void on(Node result) {
                             double temp = random.nextDouble() * 10 + 15; //random between 15 and 25
-                            double humidity = random.nextDouble() * 5 + 5; //random between 5 and 10
-                            double power = random.nextDouble() * 600 + 200; //random between 200 and 800
+                            double humidity = random.nextDouble() * 10 + 15; //random between 5 and 10
+                            double power = random.nextDouble() * 10 + 15; //random between 30 and 40
 
                             final double corr = temp * coef[0] + humidity * coef[1] + power * coef[2] + coef[3];
 
@@ -62,23 +65,47 @@ public class LiveLinearRegressionTest {
 
                 }
 
-                final double[] weights = new double[coef.length];
-                learningNode.jump(Constants.END_OF_TIME, new Callback<Node>() {
-                    @Override
-                    public void on(Node result) {
-                        double[] w =(double[])result.get(LiveLinearRegressionNode.WEIGHT_KEY);
-                        for(int i=0;i<w.length;i++){
-                            weights[i]=w[i];
-                        }
-                        result.free();
-                    }
-                });
+                final double[] cumerr = new double[1];
+                cumerr[0] = 0;
 
-                double max=0;
-                for(int i=0;i<weights.length;i++){
-                    max=Math.max(max,Math.abs(weights[i]-coef[i]));
+                int test = 10000;
+                for (int i = size; i < size + test; i++) {
+                    MultiSensor.jump(i + 1, new Callback<Node>() {
+                        @Override
+                        public void on(Node result) {
+                            double temp = random.nextDouble() * 10 + 15; //random between 15 and 25
+                            double humidity = random.nextDouble() * 5 + 5; //random between 5 and 10
+                            double power = random.nextDouble() * 600 + 200; //random between 200 and 800
+
+                            final double corr = temp * coef[0] + humidity * coef[1] + power * coef[2] + coef[3];
+
+                            result.set("temperature", temp);
+                            result.set("humidity", humidity);
+                            result.set("power", power);
+
+                            result.rel("regression", new Callback<Node[]>() {
+                                @Override
+                                public void on(Node[] result) {
+                                    RegressionNode regressionNode = (RegressionNode) (result[0]);
+                                    regressionNode.extrapolate(new Callback<Double>() {
+                                        @Override
+                                        public void on(Double result) {
+                                            cumerr[0] += Math.abs(result - corr);
+                                        }
+                                    });
+                                    regressionNode.free();
+                                }
+                            });
+
+                            result.free();
+                        }
+                    });
+
                 }
-                System.out.println(max);
+
+                cumerr[0] = cumerr[0] / test;
+
+                System.out.println("Avg error: " + cumerr[0]);
                 graph.disconnect(null);
             }
         });
