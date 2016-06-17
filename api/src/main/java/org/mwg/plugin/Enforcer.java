e7e37a93cd5b32d6ef10af0a946d57a6da826515
Assaad MOAWAD <assaad.moawad@uni.lu>
2016-06-17 14:18:49 +0200

    Update enforcer

--- api/src/main/java/org/mwg/plugin/Enforcer.java
+++ api/src/main/java/org/mwg/plugin/Enforcer.java
@@ -14,7 +14,7 @@ public class Enforcer {
             @Override
             public void check(byte inputType, Object input) throws RuntimeException {
                 if (input != null && inputType != Type.BOOL) {
-                    throw new RuntimeException("Property " + propertyName + " should be long value, currently " + input);
+                    throw new RuntimeException("Property " + propertyName + " should be Boolean value, currently " + input);
                 }
             }
         });
@@ -25,7 +25,7 @@ public class Enforcer {
             @Override
             public void check(byte inputType, Object input) throws RuntimeException {
                 if (input != null && inputType != Type.STRING) {
-                    throw new RuntimeException("Property " + propertyName + " should be long value, currently " + input);
+                    throw new RuntimeException("Property " + propertyName + " should be String value, currently " + input);
                 }
             }
         });
@@ -106,6 +106,28 @@ public class Enforcer {
             public void check(byte inputType, Object input) throws RuntimeException {
                 if (input != null && inputType != Type.DOUBLE_ARRAY) {
                     throw new RuntimeException("Property " + propertyName + " should be doubleArray value, currently " + input);
+                }
+            }
+        });
+    }
+
+    public Enforcer asPositiveInt(String propertyName) {
+        return declare(propertyName, new EnforcerChecker() {
+            @Override
+            public void check(byte inputType, Object input) throws RuntimeException {
+                if (input != null && inputType != Type.INT && inputType<=0) {
+                    throw new RuntimeException("Property " + propertyName + " should be a positive integer, currently " + input);
+                }
+            }
+        });
+    }
+
+    public Enforcer asPositiveDouble(String propertyName) {
+        return declare(propertyName, new EnforcerChecker() {
+            @Override
+            public void check(byte inputType, Object input) throws RuntimeException {
+                if (input != null && inputType != Type.DOUBLE && inputType<=0) {
+                    throw new RuntimeException("Property " + propertyName + " should be a positive double, currently " + input);
                 }
             }
         });
