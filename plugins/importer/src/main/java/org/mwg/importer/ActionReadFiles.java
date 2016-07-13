a5ea4b8a1eaff5bcda7a6a42c8c2102ed6fe59a4
Ludovic Mouline <ludovic.mouline@uni.lu>
2016-07-13 07:57:47 +0200

    now ActionReadFiles return array of String

--- plugins/importer/src/main/java/org/mwg/importer/ActionReadFiles.java
+++ plugins/importer/src/main/java/org/mwg/importer/ActionReadFiles.java
@@ -3,6 +3,9 @@ package org.mwg.importer;
 import org.mwg.task.TaskAction;
 import org.mwg.task.TaskContext;
 
+import java.io.File;
+import java.net.URL;
+
 class ActionReadFiles implements TaskAction {
 
     private final String _pathOrTemplate;
@@ -20,13 +23,37 @@ class ActionReadFiles implements TaskAction {
         if (path == null) {
             throw new RuntimeException("Variable " + _pathOrTemplate + " does not exist in the context");
         }
-        res = new IterableFiles(path);
+        res = getFilesPaths(path);
         context.setUnsafeResult(res);
+    }
+
+    private String[] getFilesPaths(String path) {
+        String[] filesPath;
+        File file = new File(path);
+        if(!file.exists()) {
+            URL url = this.getClass().getClassLoader().getResource(path);
+            if(url == null) {
+                throw new RuntimeException("File " + path + " does not exist and it is not present in resources directory.");
+            }
+            file = new File(url.getPath());
+        }
+
+        if(file.isDirectory()) {
+            File[] listFiles = file.listFiles();
+            filesPath = new String[listFiles.length];
+            for(int i=0;i<listFiles.length;i++) {
+                filesPath[i] = listFiles[i].getAbsolutePath();
+            }
+        } else {
+            filesPath = new String[] {file.getAbsolutePath()};
+        }
+        return filesPath;
     }
 
     @Override
     public String toString() {
         return "readFiles(\'" + _pathOrTemplate + "\')";
     }
+
 
 }
