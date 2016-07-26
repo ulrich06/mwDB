e9b61f3a575d20f412b623ad977836881ddf46c4
Assaad MOAWAD <assaad.moawad@uni.lu>
2016-07-26 17:00:57 +0200

    update test

--- core/src/test/java/org/mwg/core/task/ActionWhileDoTest.java
+++ core/src/test/java/org/mwg/core/task/ActionWhileDoTest.java
@@ -16,23 +16,34 @@ public class ActionWhileDoTest extends AbstractActionTest {
             @Override
             public void on(Node root) {
 
-                Task whiletask = newTask().inject(root).asGlobalVar("parent").whileDo(new TaskFunctionConditional() {
+
+                Task whiletask = newTask().inject(root).whileDo(new TaskFunctionConditional() {
                     @Override
                     public boolean eval(TaskContext context) {
-                        //System.out.println("condition");
-                        return false;
+                        System.out.println("condition while");
+                        return context.result().size() != 0;
                     }
-                }, then(new Action() {
+                }, foreach(ifThenElse(new TaskFunctionConditional() {
+                            @Override
+                            public boolean eval(TaskContext context) {
+                                System.out.println("condition if");
+                                Node res= (Node) context.result().get(0);
+                                long[] c= (long[]) res.get("child");
+                                return (c!=null && c.length>0);
+                            }
+                        }, traverse("child")
+                        , then(new Action() {
+                            @Override
+                            public void eval(TaskContext context) {
+                                System.out.println("if is false");
+                                context.addToGlobalVariable("leaves",context.result());
+                                context.continueWith(null);
+                            }
+                        })))
+                ).then(new Action() {
                     @Override
                     public void eval(TaskContext context) {
-                        //System.out.println("not printed");
-                        context.continueTask();
-
-                    }
-                })).then(new Action() {
-                    @Override
-                    public void eval(TaskContext context) {
-                        //System.out.println("outside");
+                        System.out.println("done with "+context.result().size());
                         context.continueTask();
                     }
                 });
@@ -41,17 +52,13 @@ public class ActionWhileDoTest extends AbstractActionTest {
                 whiletask.execute(graph, new Callback<TaskResult>() {
                     @Override
                     public void on(TaskResult result) {
-                        //System.out.println("end");
+                        System.out.println("end "+result.size());
                     }
                 });
 
 
-
-
             }
         });
-
-
 
 
     }
