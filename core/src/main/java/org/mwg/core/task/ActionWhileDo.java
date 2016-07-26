793c3b7860eace6da204bc3ce96daac4c3cc7d89
Francois Fouquet <fouquet.f@gmail.com>
2016-07-26 18:06:48 +0200

    fixing impl of whileDo

--- core/src/main/java/org/mwg/core/task/ActionWhileDo.java
+++ core/src/main/java/org/mwg/core/task/ActionWhileDo.java
@@ -2,11 +2,8 @@ package org.mwg.core.task;
 
 import org.mwg.Callback;
 import org.mwg.plugin.AbstractTaskAction;
-import org.mwg.plugin.Job;
 import org.mwg.plugin.SchedulerAffinity;
 import org.mwg.task.*;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 class ActionWhileDo extends AbstractTaskAction {
 
@@ -22,54 +19,30 @@ class ActionWhileDo extends AbstractTaskAction {
     @Override
     public void eval(final TaskContext context) {
         final ActionWhileDo selfPointer = this;
-
-        final TaskResult previousResult = context.result();
-
-        if (previousResult == null) {
-            context.continueTask();
-        } else {
-            final TaskResultIterator it = previousResult.iterator();
-            final TaskResult finalResult = context.wrap(null);
-            finalResult.allocate(previousResult.size());
-            final AtomicInteger cursor = new AtomicInteger(0);
-            final Callback[] recursiveAction = new Callback[1];
-            final TaskResult[] loopRes = new TaskResult[1];
-            recursiveAction[0] = new Callback<TaskResult>() {
-                @Override
-                public void on(final TaskResult res) {
-                    int current = cursor.getAndIncrement();
-                    finalResult.set(current, res);
+        final Callback[] recursiveAction = new Callback[1];
+        final TaskResult[] loopRes = new TaskResult[1];
+        recursiveAction[0] = new Callback<TaskResult>() {
+            @Override
+            public void on(final TaskResult res) {
+                if (_cond.eval(context)) {
                     loopRes[0].free();
-                    Object nextResult = it.next();
-                    if (_cond.eval(context)) {
-                        loopRes[0] = context.wrap(it.next());
-                    } else {
-                        loopRes[0] = null;
-                    }
-                    if (nextResult == null) {
-                        context.continueWith(finalResult);
-                    } else {
-                        context.graph().scheduler().dispatch(SchedulerAffinity.SAME_THREAD, new Job() {
-                            @Override
-                            public void run() {
-                                selfPointer._then.executeFrom(context, context.wrap(loopRes[0]), SchedulerAffinity.SAME_THREAD, recursiveAction[0]);
-                            }
-                        });
-                    }
+                    loopRes[0] = res;
+                    selfPointer._then.executeFrom(context, context.wrap(loopRes[0]), SchedulerAffinity.SAME_THREAD, recursiveAction[0]);
+                } else {
+                    context.continueWith(res);
                 }
-            };
-            if (_cond.eval(context)) {
-                loopRes[0] = context.wrap(it.next());
-                context.graph().scheduler().dispatch(SchedulerAffinity.SAME_THREAD, new Job() {
-                    @Override
-                    public void run() {
-                        _then.executeFrom(context, context.wrap(loopRes[0]), SchedulerAffinity.SAME_THREAD, recursiveAction[0]);
-                    }
-                });
-            } else {
-                context.continueWith(finalResult);
             }
+        };
+        if (_cond.eval(context)) {
+            loopRes[0] = context.result();
+            _then.executeFrom(context, loopRes[0], SchedulerAffinity.SAME_THREAD, recursiveAction[0]);
+        } else {
+            context.continueTask();
         }
     }
 
+    @Override
+    public String toString() {
+        return "whileDo()";
+    }
 }
--- core/src/test/java/org/mwg/core/task/ActionWhileDoTest.java
+++ core/src/test/java/org/mwg/core/task/ActionWhileDoTest.java
@@ -4,6 +4,7 @@ package org.mwg.core.task;
 import org.junit.Test;
 import org.mwg.Callback;
 import org.mwg.Node;
+import org.mwg.plugin.ConsoleHook;
 import org.mwg.task.*;
 
 import static org.mwg.task.Actions.*;
@@ -20,36 +21,33 @@ public class ActionWhileDoTest extends AbstractActionTest {
                 Task whiletask = newTask().inject(root).whileDo(new TaskFunctionConditional() {
                     @Override
                     public boolean eval(TaskContext context) {
-                        System.out.println("condition while");
+                        //System.out.println("condition while");
                         return context.result().size() != 0;
                     }
                 }, foreach(ifThenElse(new TaskFunctionConditional() {
                             @Override
                             public boolean eval(TaskContext context) {
-                                System.out.println("condition if");
-                                Node res= (Node) context.result().get(0);
-                                long[] c= (long[]) res.get("child");
-                                return (c!=null && c.length>0);
+                                return context.resultAsNodes().get(0).get("child") != null;
                             }
                         }, traverse("child")
                         , then(new Action() {
                             @Override
                             public void eval(TaskContext context) {
-                                System.out.println("if is false");
+                                //System.out.println("if is false");
                                 context.addToGlobalVariable("leaves",context.result());
                                 context.continueWith(null);
                             }
                         })))
-                ).then(new Action() {
+                )/*.fromVar("leaves")*/.then(new Action() {
                     @Override
                     public void eval(TaskContext context) {
-                        System.out.println("done with "+context.result().size());
+                        //System.out.println("done with "+context.result().size());
                         context.continueTask();
                     }
                 });
 
 
-                whiletask.execute(graph, new Callback<TaskResult>() {
+                whiletask.hook(ConsoleHook.instance()).execute(graph, new Callback<TaskResult>() {
                     @Override
                     public void on(TaskResult result) {
                         System.out.println("end "+result.size());
